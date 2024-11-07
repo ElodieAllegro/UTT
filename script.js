8c062cc8340a1afe441d12d5c0e903bf7106c547
ElodieAllegro <124352648+ElodieAllegro@users.noreply.github.com>
2024-11-07 13:14:03 +0100

    parallax

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ document.querySelector('.burger-menu').addEventListener('click', function() {
     console.log('click');
     document.querySelector('.menu').classList.toggle('active');
 });
+const image = document.querySelectorAll(".img");
+
+window.addEventListener("scroll", () => {
+    const scrollAmount = window.scrollY;
+  
+    image.forEach((image) => {
+      image.style.transform = `translateY(${scrollAmount * -0.03}px)`; // Facteur réduit pour un effet plus léger
+    });
+  });
